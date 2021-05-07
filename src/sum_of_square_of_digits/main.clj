335f7dc9cff9cf0a67ed0c4508f8190b8c2bb38c
Jimmy <jimmycanosajr@gmail.com>
2021-05-07 22:44:11 +0800

    Add new solution to Sum of Square of Digits

--- src/sum_of_square_of_digits/main.clj
+++ src/sum_of_square_of_digits/main.clj
@@ -1,12 +1,12 @@
 (ns sum-of-square-of-digits)
 
-(= 8 (f1 (range 10)))
+(= 8 (f2 (range 10)))
 
-(= 19 (f1 (range 30)))
+(= 19 (f2 (range 30)))
 
-(= 50 (f1 (range 100)))
+(= 50 (f2 (range 100)))
 
-(= 50 (f1 (range 1000)))
+(= 50 (f2 (range 1000)))
 
 (def f1
   (fn [coll]
@@ -16,3 +16,6 @@
                ([n] (if (< n 10) (* n n) (f (quot n 10) (* (mod n 10) (mod n 10)))))
                ([m n] (if (zero? m) n (recur (quot m 10) (+ n (* (mod m 10) (mod m 10)))))))
              %)) coll))))
+
+(def f2
+  #(reduce + (for [n % :when (< n (reduce + (for [c (str n)] (* (- (int c) 48) (- (int c) 48)))))] 1)))
